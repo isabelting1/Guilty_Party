e48f5cb4d81487f30b37573365c6f47fdbd99d17
Isabel Ting <isabelting@icloud.com>
2020-05-29 11:19:36 -0400

    Initial Commit: Adding files and testing. May add other files later.

---
+++ desktop/src/com/guilty/party/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.guilty.party.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.guilty.party.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
